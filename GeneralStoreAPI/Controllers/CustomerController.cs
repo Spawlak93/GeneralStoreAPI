77d5428faad55511a3afd05aa41820f153653929
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-08-27 11:06:40 -0400

    Add project files.

---
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,85 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        //Crud
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        //Create New Customers(POST)
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateCustomer(Customer model)
+        {
+            if (model is null)
+                return BadRequest("Your request cannot be empty");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Customers.Add(model);
+            await _context.SaveChangesAsync();
+            return Ok("Customer Added");
+        }
+
+        //All Customers(GET)
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllCustomers()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+        //Single Customer(GET)
+        [HttpGet]
+        public async Task<IHttpActionResult> GetCustomerById(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+                return Ok(customer);
+            return NotFound();
+        }
+
+        //Update existing customer By ID(Put)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateCustomerById([FromUri]int id,[FromBody] Customer updatedCustomer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Customer customer = await _context.Customers.FindAsync(id);
+            if (customer is null)
+                return NotFound();
+
+            customer.FirstName = updatedCustomer.FirstName;
+            customer.LastName = updatedCustomer.LastName;
+
+            await _context.SaveChangesAsync();
+            return Ok($"Customer {id} updated");
+        }
+
+        //Delete Customer By ID(DELETE)
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteCustomerById(int id)
+        {
+            Customer entity = await _context.Customers.FindAsync(id);
+
+            if (entity is null)
+                return NotFound();
+
+            _context.Customers.Remove(entity);
+
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok($"Customer {id} Deleted");
+
+            return InternalServerError();
+        }
+    }
+}
---
+++ GeneralStoreAPI/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+    }
+}
